d76b9ae00d4616a58ff86cd80ca5a0e5c9a0b3c3
Evan Hubinger <evanjhub@gmail.com>
2019-03-10 13:33:08 -0700

    Bump version

--- bbopt/constants.py
+++ bbopt/constants.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# __coconut_hash__ = 0xa48dad1b
+# __coconut_hash__ = 0x6c28b882
 
 # Compiled with Coconut version 1.4.0-post_dev23 [Ernest Scribbler]
 
@@ -29,7 +29,7 @@ if _coconut_sys.version_info >= (3,):
 
 # Installation constants:
 name = "bbopt"
-version = "0.4.5"
+version = "0.4.6"
 description = "The easiest hyperparameter optimization you'll ever do."
 github_url = "https://github.com/evhub/bbopt"
 author = "Evan Hubinger"
